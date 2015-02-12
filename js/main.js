5ca68c9db394ca294aa4b551a1257406afb0d32a
Heron Yang <heron.yang.tw@gmail.com>
2015-02-12 03:21:25 -0500

    update new version

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
+/*
 $(".link").hover(function(){
     console.log("animtion fired");
     $(this).animate({ color: '#B22222'}, "slow");
 }, function() {
     $(this).animate({ color: '#666'}, "slow"); //original color
+});
+*/
+$(document).ready(function(){
+    $('.slider').slider({full_width: true});
+    $(".button-collapse").sideNav();
 });
